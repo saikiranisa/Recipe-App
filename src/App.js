605258010dc56ebb6140b659b67d1b2fb41dba71
Isa saikiran <88225022+saikiranisa@users.noreply.github.com>
2022-08-03 19:49:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+
+
+import Header from './components/headerComponent';
+import Recipe from './components/recipeComponent';
+import Image from './components/headerComponent';
+
+
+const APP_ID = "a52b4d43";
+const APP_KEY = "e0e5c667605f5e91d8275c973531b80a";
+
+const RecipeComponent = (props) => {
+  const [show, setShow] = useState("");
+
+  const { label, image, ingredients, url } = props.recipe;
+  return (
+    <Recipe.RecipeContainer>
+      <Dialog
+        onClose={() => console.log("Container Closed")}
+        aria-labelledby="simple-dialog-title"
+        open={!!show}
+      >
+        <DialogTitle>Ingredients</DialogTitle>
+        <DialogContent>
+          <Recipe.RecipeName>{label}</Recipe.RecipeName>
+          <table>
+            <thead>
+              <th>Ingredient</th>
+              <th>Weight</th>
+            </thead>
+            <tbody>
+              {ingredients.map((ingredient, index) => (
+                <tr key={index} className="ingredient-list">
+                  <td>{ingredient.text}</td>
+                  <td>{ingredient.weight}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </DialogContent>
+        <DialogActions>
+          <Recipe.SeeNewTab onClick={() => window.open(url)}>See More</Recipe.SeeNewTab>
+          <Recipe.SeeMoreText onClick={() => setShow("")}>Close</Recipe.SeeMoreText>
+        </DialogActions>
+      </Dialog>
+      <Recipe.RecipeName>{label}</Recipe.RecipeName>
+      <Recipe.CoverImage src={image} alt={label} />
+      <Recipe.IngredientsText onClick={() => setShow(!show)}>
+        Ingredients List
+      </Recipe.IngredientsText>
+      <Recipe.SeeMoreText onClick={() => window.open(url)}>
+        Full Recipe
+      </Recipe.SeeMoreText>
+    </Recipe.RecipeContainer>
+  );
+};
+const AppComponent = () => {
+  const [searchQuery, updateSearchQuery] = useState("");
+  const [recipeList, updateRecipeList] = useState([]);
+  const [timeoutId, updateTimeoutId] = useState();
+  const fetchData = async (searchString) => {
+    const response = await Axios.get(
+      `https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`,
+    );
+  
+  
+    updateRecipeList(response.data.hits);
+  };
+
+  const onTextChange = (e) => {
+    clearTimeout(timeoutId);
+    updateSearchQuery(e.target.value);
+      const timeout = setTimeout(() => fetchData(e.target.value), 500);
+      updateTimeoutId(timeout);
+  };
+  return (
+    <Header.Container>
+      <Header.Header>
+        
+        <Header.SearchBox>
+          <Header.SearchIcon src='/images/Search.svg'/>
+          <Header.SearchInput
+            placeholder="Search"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+
+
+        </Header.SearchBox>
+      </Header.Header>
+      <Header.Header >
+        
+        <em>
+        Refine search by....
+        </em>
+        
+      </Header.Header>
+      <Header.RecipeListContainer>
+        {recipeList?.length ? (
+          recipeList.map((recipe, index) => (
+            <RecipeComponent key={index} recipe={recipe.recipe} />
+          ))
+        ) : (
+          <Image.Placeholder src="/images/original-recipe.svg"/>
+        )}
+      </Header.RecipeListContainer>
+    </Header.Container>
+  );
+};
+
+export default AppComponent;
